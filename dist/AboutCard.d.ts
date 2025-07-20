45d927538eeaa8faad2c44b6359307b7a594c8b3
oh-Seongsik <blackvrice@naver.com>
2025-07-20 13:32:48 +0900

    빌드 내용 추가

---
+++ dist/AboutCard.d.ts
@@ -0,0 +1,6 @@
+import React from "react";
+declare const _default: ({ className, content }: {
+    className: string;
+    content: Element;
+}) => React.JSX.Element;
+export default _default;
---
+++ dist/AboutCard.js
@@ -0,0 +1,4 @@
+import React from "react";
+export default ({ className, content }) => {
+    return (React.createElement("div", { className: className }, "content"));
+};
